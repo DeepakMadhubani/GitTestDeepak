66b10c9e7c5748901921cf59fd84b03b7a25f609
yc03dk1 <Deepak.kumar@gspann.com>
2016-05-04 18:33:14 +0530

    First Commit

---
+++ test1.java
@@ -0,0 +1,20 @@
+package com.macys.stella.test.fasttrack;
+
+import org.jfree.util.Log;
+
+public class test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		String issueNumber="Halted (00895892)";
+		String[]isn1=issueNumber.split("\\(");
+		System.out.println("1st split---"+isn1[0]+ "and..." +isn1[1] );
+		String[] isn2=isn1[1].split("\\)");
+		System.out.println("1st split---"+isn2[0] );
+
+	}
+
+}
